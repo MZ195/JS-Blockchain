90e3e541c72c6685093dea2ead312afa2cca8fd6
Mazen.A <40984264+MZ195@users.noreply.github.com>
2020-07-07 11:10:30 +0300

    API endpoints are ready
    New changes:
    - /resigter-node will registering a new node in the network
    - /register-and-broadcast-node will register and broadcast the new node
    - /register-nodes-bulk will register all network nodes in the new node

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -81,21 +81,47 @@ app.post('/register-and-broadcast-node', function(req, res) {
         regNodesPromises.push(rp(requestOptions))
     })
 
+    // promise all will excute all the requests in regNodesPromises
     Promise.all(regNodesPromises)
         .then(data => {
-            // returned responses
+            // this chunk of code will be excuted after all requests are completed successfully
+            // we will be bulk registering all network nodes to the new node
+            const bulkRegisterOptions = {
+                uri: newNodeURL + '/register-nodes-bulk',
+                method: 'POST',
+                body: {allNetworkNodes: [...bitcoin.networkNodes, bitcoin.currentNodeURL]}
+            }
+            rp(bulkRegisterOptions)
         })
 
-})
+        .then(data => {
+            // we will send back a note to the new node
+            res.json({
+                note: 'New Node registered with network successfully'
+            })
+        })
+    })
 
 // register the node on the network
 app.post('/resigter-node', function(req, res) {
+    const newNodeURL = req.body.newNodeURL
 
+    // check if the node already exists and the new node's url is not of this node
+    if(bitcoin.networkNodes.indexOf(newNodeURL) == -1 && bitcoin.currentNodeURL !== newNodeURL)
+        bitcoint.networkNodes.push(newNodeURL)
+
+    res.json({
+        note: 'New Node registered successfully.'
+    })
 })
 
 // register multiple nodes at once in the new node
 app.post('/register-nodes-bulk', function(req, res) {
-
+    const allNetworkNodes = req.body.allNetworkNodes
+    allNetworkNodes.forEach(networkNodeURL => {
+        if(bitcoin.networkNodes.indexOf(networkNodeURL) == -1 && bitcoin.currentNodeURL !== newNodeURL)
+            bitcoin.networkNodes.push(networkNodeURL)
+    })
 })
 
 app.listen(port, function () {
